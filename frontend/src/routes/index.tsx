fe0238867258e9ce4b4c1e3495d509a54f668434
Justin Solo <justin@justinsolo.com>
2026-03-26 15:27:46 -0700

    ✨ feat: add date picer for pickup and dropoff dates

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,19 +1,64 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { useState } from 'react'
 import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import { Field, FieldLabel } from '@/components/ui/field'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { createFileRoute } from '@tanstack/react-router'
+import { addDays, format } from 'date-fns'
+import { CalendarIcon } from 'lucide-react'
+import { useState } from 'react'
+import type { DateRange } from 'react-day-picker'
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
 function Index() {
-  const [count, setCount] = useState(0)
+  const [date, setDate] = useState<DateRange | undefined>({
+    from: new Date(new Date().getFullYear(), 0, 20),
+    to: addDays(new Date(new Date().getFullYear(), 0, 20), 20),
+  })
 
   return (
-    <div className="p-2">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button onClick={() => setCount(count + 1)}>Click me</Button>
-      <p>You clicked {count} times</p>
+    <div className="sticky top-[65px] z-40 bg-background border-b border-border">
+        <div className="max-w-7xl mx-auto px-4 py-4">
+          <div className="flex flex-col gap-4 md:flex-row md:items-end md:justify-between">
+      <Field className="flex w-60">
+        <FieldLabel htmlFor="date-picker-range">Pick-up & Return Dates</FieldLabel>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              id="date-picker-range"
+              className="justify-start px-2.5 font-normal"
+            >
+              <CalendarIcon />
+              {date?.from ? (
+                date.to ? (
+                  <>
+                    {format(date.from, "LLL dd, y")} -{" "}
+                    {format(date.to, "LLL dd, y")}
+                  </>
+                ) : (
+                  format(date.from, "LLL dd, y")
+                )
+              ) : (
+                <span>Pick a date</span>
+              )}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar
+              mode="range"
+              defaultMonth={date?.from}
+              selected={date}
+              onSelect={setDate}
+              numberOfMonths={2}
+            />
+          </PopoverContent>
+        </Popover>
+      </Field>
+          </div>
+        </div>
     </div>
   )
 }
